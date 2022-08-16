be42f3f37bcf154bc7c99594720a82947b2103b1
Oltion <oltion.abazi@gmail.com>
2022-08-16 15:05:11 +0200

    feat(Separator): new component

---
+++ src/Separator.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { PRIMARY } from '../utils/theme';
+import { Normalize } from '../utils';
+
+export const Separator = ({ style, color = PRIMARY, size, width }) => {
+  return (
+    <View
+      style={[
+        styles.separator,
+        style,
+        { borderColor: color },
+        { borderBottomWidth: size || 1 },
+        { width: width || '100%' },
+      ]}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  separator: {
+    borderBottomWidth: 1,
+    borderBottomColor: PRIMARY,
+    marginVertical: Normalize(3),
+    width: '100%',
+  },
+});
